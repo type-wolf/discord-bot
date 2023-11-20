b43fc38c78cd92edf8d78af7a0e7aec14434c633
type-wolf <parallel.coffee.car@gmail.com>
2023-11-20 16:25:04 +0900

    Add
    Create onGuildMemberUpdate Eventhandler

---
+++ src/events/onGuildMemberUpdate/index.ts
@@ -0,0 +1,38 @@
+import { DiscordAPIError, type GuildMember, type PartialGuildMember } from 'discord.js';
+
+export type OnGuildMemberUpdateOptions = {
+	//
+};
+
+/**
+ * ------------------------------------------
+ * @name - onGuildMemberUpdate
+ * ------------------------------------------
+ * @description - Event executed when a Member of the Guild in which the Bot is participating updates his/her profile
+ * ------------------------------------------
+ * @param - oldState: Complete or incomplete objects of old members
+ * ------------------------------------------
+ * @param - newState: Objects with complete information about the new user
+ * ------------------------------------------
+ * @param - options?: Arbitrary arguments that this event receives
+ * ------------------------------------------
+ **/
+
+const onGuildMemberUpdate = async (
+	oldState: GuildMember | PartialGuildMember,
+	newState: GuildMember,
+	options?: OnGuildMemberUpdateOptions
+) => {
+	try {
+		return;
+	} catch (e: unknown) {
+		if (e instanceof DiscordAPIError) {
+			return;
+		}
+		if (e instanceof Error) {
+			return;
+		}
+	}
+};
+
+export default onGuildMemberUpdate;
