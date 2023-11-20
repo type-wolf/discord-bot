1cdba45153d2e520db610caf349117d6529b03f6
type-wolf <parallel.coffee.car@gmail.com>
2023-11-20 16:25:34 +0900

    Add
    Create onGuildScheduledEventUpdate EventHandler

---
+++ src/events/onGuildScheduledEventUpdate/index.ts
@@ -0,0 +1,38 @@
+import { DiscordAPIError, type GuildScheduledEvent } from 'discord.js';
+
+export type OnGuildScheduledEventUpdateOptions = {
+	//
+};
+
+/**
+ * ------------------------------------------
+ * @name - onGuildScheduledEventUpdate
+ * ------------------------------------------
+ * @description - Runs when the content of the Guild event in which the bot is participating is updated
+ * ------------------------------------------
+ * @param - oldState: Receive the complete object of the event before updating
+ * ------------------------------------------
+ * @param - newState: Receive the complete object of the event after the update
+ * ------------------------------------------
+ * @param - options?: Arbitrary arguments that this event receives
+ * ------------------------------------------
+ **/
+
+const onGuildScheduledEventUpdate = async (
+	oldState: GuildScheduledEvent,
+	newState: GuildScheduledEvent,
+	options?: OnGuildScheduledEventUpdateOptions
+) => {
+	try {
+		return;
+	} catch (e: unknown) {
+		if (e instanceof DiscordAPIError) {
+			return;
+		}
+		if (e instanceof Error) {
+			return;
+		}
+	}
+};
+
+export default onGuildScheduledEventUpdate;
