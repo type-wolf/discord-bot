60beb930e06b6932a91bdfcd2252e2ff57740499
type-wolf <parallel.coffee.car@gmail.com>
2023-11-20 16:31:16 +0900

    Add
    Create onGuildScheduledEventDelete EventHandler

---
+++ src/events/onGuildScheduledEventDelete/index.ts
@@ -0,0 +1,32 @@
+import { DiscordAPIError, type GuildScheduledEvent } from 'discord.js';
+
+export type OnGuildScheduledEventDeleteOptions = {
+	//
+};
+
+/**
+ * ------------------------------------------
+ * @name - onGuildScheduledEventDelete
+ * ------------------------------------------
+ * @description - Executed when an event in the Guild in which the bot is participating is deleted
+ * ------------------------------------------
+ * @param - schedule: Receive the complete object of the deleted schedule
+ * ------------------------------------------
+ * @param - options?: Arbitrary arguments that this event receives
+ * ------------------------------------------
+ **/
+
+const onGuildScheduledEventDelete = async (schedule: GuildScheduledEvent, options?: OnGuildScheduledEventDeleteOptions) => {
+	try {
+		return;
+	} catch (e: unknown) {
+		if (e instanceof DiscordAPIError) {
+			return;
+		}
+		if (e instanceof Error) {
+			return;
+		}
+	}
+};
+
+export default onGuildScheduledEventDelete;
